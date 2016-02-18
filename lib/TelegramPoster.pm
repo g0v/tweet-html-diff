2a8b6a74c5bb2009d21e7007f44b126b9b60eb1d
Kang-min Liu <gugod@gugod.org>
2016-02-18 22:21:22 +0100

    remove async callbacks.

--- lib/TelegramPoster.pm
+++ lib/TelegramPoster.pm
@@ -1,26 +1,29 @@
 package TelegramPoster;
 use v5.14;
+use strict;
+use warnings;
 use WWW::Telegram::BotAPI;
 
 sub new {
     my $class = shift;
     my %args  = @_;
-    my $bot = WWW::Telegram::BotAPI->new (token => $args{token});
+    my $bot = WWW::Telegram::BotAPI->new( token => $args{token});
+    my $tx = $bot->api_request('getMe');
+    $tx = $bot->api_request('getUpdates', { offset => 0 });
+
     return bless { bot => $bot, chat_id => $args{chat_id} }, $class;
 }
 
 sub post {
     my ($self, $content) = @_;
-
+    say "POSTING << $content";
     $self->{bot}->api_request(
         sendMessage => {
             chat_id => $self->{chat_id},
             text    => $content
-        }, sub {
-            my ($ua, $tx) = @_;
-            return unless $tx->success;
         }
     );
+    return $self;
 }
 
 1;
